4085334641a3cf4b0f5ed940346c8636e384e835
Corin V <Corin V@DESKTOP-SV8RA5T>
2019-09-02 18:08:33 -0500

    Arreglos de metodos

--- src/main/java/com/udec/ejerciciopolimorfismo/Logica.java
+++ src/main/java/com/udec/ejerciciopolimorfismo/Logica.java
@@ -30,83 +30,102 @@ public class Logica {
             System.out.println("Escoja una figura");
             System.out.println("1. Triangulo ");
             System.out.println("2. Cuadrado");
-            System.out.println("3. Rectangulo");
-            System.out.println("4. Circulo");
-            System.out.println("5. Piramide");
-            System.out.println("6. Cubo");
-            System.out.println("7. Prisma");
-            System.out.println("8. Esfera");   
+            System.out.println("3. Circulo");
+            System.out.println("4. Piramide");
+            System.out.println("5. Cubo");
+            System.out.println("6. Esfera");   
             System.out.print("Seleccione "); 
             seleccion=ingreso.nextShort();
+            System.out.println("");
             validarSeleccion(seleccion,posicion++);
             System.out.println("Desea Continuar s/n"); 
         }while(Character.toLowerCase(ingreso.next().charAt(0))=='s');
         calculos();
     }
     /**
-     * Metodo que valida el tipo de figura si es 2D o 3D
+     * Metodo que valida la selección
      * @param seleccion segun lo escogido por el usuario
      * @param posicion del vector figura
      */
     private void validarSeleccion(short seleccion,int posicion) {
-        if(seleccion<=4){
-            llenarFiguras2D(seleccion,posicion);
-        }else{
-            llenarFiguras3D(seleccion,posicion);
-        }
-    
+       switch(seleccion){
+           case 1:
+               llenarTriangulo(posicion);
+               break;
+           case 2:
+               llenarCuadrado(posicion);
+               break;
+           case 3: 
+               llenarCirucloEclipse(posicion, "Circulo");
+               break;
+           case 4:
+               llenarPiramide(posicion);
+               break;
+           case 5:
+               llenarCubo(posicion);
+               break;
+           case 6:
+               llenarCirucloEclipse(posicion,"Elipse");
+               break;
+       }   
     }
     /**
-     * En este metodo se solicitan los datos al usuario con respecto a las Figuras 2D y se llena el vector figura
-     * @param seleccion segun lo escogido por el usuario
+     * Metodo para solicitar los datos del triangulo
+     * @param posicion del vector figura
+     */
+    private void llenarTriangulo(int posicion){
+        System.out.print("Inserte lado 1: ");
+        int lado1=ingreso.nextInt();
+        System.out.print("Inserte lado 2: ");
+        int lado2=ingreso.nextInt();
+        System.out.print("Inserte lado 3: ");
+        int lado3=ingreso.nextInt();
+        figura[posicion]=new Triangulo(lado1,lado2,lado3);
+    }/**
+     * Solicitar datos del cuadrado o rectangulo
      * @param posicion del vector
      */
-    private void llenarFiguras2D(short seleccion,int posicion) {   
-        switch(seleccion){
-                case 1:{                
-                    System.out.println("Inserte los tres lados del triangulo:");
-                    figura[posicion]=new Triangulo(ingreso.nextInt(),ingreso.nextInt(),ingreso.nextInt());
-                    break;
-                }
-                case 2:
-                case 3:{
-                    System.out.println("Inserte los dos lados del cuadrado:");
-                    figura[posicion]=new Cuadrado(ingreso.nextInt(),ingreso.nextInt());                
-                    break;
-                }
-                case 4:{
-                    System.out.println("Inserte el radio:");
-                    figura[posicion]=new Circulo(ingreso.nextDouble());                
-                    break;
-                }
-            }   
-    
+    private void llenarCuadrado(int posicion){
+        System.out.print("Inserte largo: ");
+        int largo=ingreso.nextInt();
+        System.out.print("Inserte ancho: ");
+        int ancho=ingreso.nextInt();
+        figura[posicion]=new Cuadrado(largo,ancho);  
+              
+    }
+    private void llenarCirucloEclipse(int posicion, String tipo){
+        System.out.print("Inserte Radio: ");
+        int radio=ingreso.nextInt();
+        if("Circulo".equals(tipo))
+            figura[posicion]=new Circulo(radio);   
+        else
+            figura[posicion]=new Esfera(radio);  
     }
     /**
-     * En este metodo se solicitan los datos al usuario con respecto a las figuras 3D y se llena el vector figura
-     * @param seleccion segun lo escogido por el usuario
+     * Metodo para solicitar datos de la figura piramide
      * @param posicion del vector
      */
-    private void llenarFiguras3D(short seleccion, int posicion) {
-        switch(seleccion){
-                case 5:{                
-                    System.out.println("Inserte area de la base, altura, area lateral:");
-                    figura[posicion]=new Piramide(ingreso.nextDouble(),ingreso.nextInt(),ingreso.nextDouble());
-                    break;
-                }
-                case 6:
-                case 7:{
-                    System.out.println("Inserte largo, ancho, altura :");                    
-                    figura[posicion]=new Cubo(ingreso.nextInt(),ingreso.nextInt(),ingreso.nextInt());                
-                    break;
-                }
-                case 8:{
-                    System.out.println("Inserte el radio:");
-                    figura[posicion]=new Esfera(ingreso.nextDouble());                
-                    break;
-                }
-            } 
-    
+    private void llenarPiramide(int posicion){
+        System.out.print("Inserte Area de la base: ");
+        double areaBase=ingreso.nextDouble();
+        System.out.print("Inserte altura: ");
+        int altura=ingreso.nextInt();
+        System.out.print("Inserte Area lateral: ");
+        double areaLateral=ingreso.nextDouble();
+        figura[posicion]=new Piramide(areaBase,altura,areaLateral);
+    }
+    /**
+     * Metodo para solicitar datos de la figura cubo
+     * @param posicion del vector
+     */
+    private void llenarCubo(int posicion){
+        System.out.print("Inserte largo: ");
+        int largo=ingreso.nextInt();
+        System.out.print("Inserte ancho: ");
+        int ancho=ingreso.nextInt();
+        System.out.print("Inserte altura: ");
+        int alto=ingreso.nextInt();
+        figura[posicion]=new Cubo(largo,ancho,alto);  
     }
     /**
      * Metodo encargado de llamar a los metodos para los calculos geometricos
